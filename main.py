7d9fb460899d96e1148ad55c11defba89f735f70
Xu, Joseph [ETHUS NON-J&J] <JXu122@its.jnj.com>
2022-08-23 03:21:04 -0400

    hack for stability

--- main.py
+++ main.py
@@ -727,7 +727,8 @@ if __name__ == "__main__":
         # configure learning rate
         bs, base_lr = config.data.params.batch_size, config.model.base_learning_rate
         if not cpu:
-            ngpu = len(lightning_config.trainer.gpus.strip(",").split(','))
+            #ngpu = len(lightning_config.trainer.gpus.strip(",").split(','))
+            ngpu = 1
         else:
             ngpu = 1
         if 'accumulate_grad_batches' in lightning_config.trainer:
@@ -791,5 +792,5 @@ if __name__ == "__main__":
             dst = os.path.join(dst, "debug_runs", name)
             os.makedirs(os.path.split(dst)[0], exist_ok=True)
             os.rename(logdir, dst)
-        if trainer.global_rank == 0:
-            print(trainer.profiler.summary())
+        #if trainer.global_rank == 0:
+        #    print(trainer.profiler.summary())
